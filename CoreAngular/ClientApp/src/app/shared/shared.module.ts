c322038efc59e12e33bbe825a3f30a0cb6b659f1
Chester Yang <chester_cyyang@hotmail.com>
2018-07-21 08:59:43 -0600

    ng g c login

--- CoreAngular/ClientApp/src/app/shared/shared.module.ts
+++ CoreAngular/ClientApp/src/app/shared/shared.module.ts
@@ -4,19 +4,24 @@ import { EmployeeViewListComponent } from '../adw/employee-vew-list/employee-vie
 import { EmployeeViewModelActions, CustomerViewModelActions } from '../adw/redux/actions';
 import { CustomerViewListComponent } from '../adw/customer-view-list/customer-view-list.component';
 import { ModelsService } from './models.service';
+import { LoginComponent } from '../adw/login/login.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    EmployeeViewListComponent, CustomerViewListComponent
+    EmployeeViewListComponent,
+    CustomerViewListComponent,
+    LoginComponent
   ],
   providers: [
     EmployeeViewModelActions, CustomerViewModelActions, ModelsService
   ],
   exports: [
-    EmployeeViewListComponent, CustomerViewListComponent
+    EmployeeViewListComponent,
+    CustomerViewListComponent,
+    LoginComponent
   ]
 })
 export class SharedModule { }
