b825201bdc752096780b6ee36150c8e297399869
Chester Yang <chester_cyyang@hotmail.com>
2018-07-23 15:21:04 -0600

    remove element from dom after karma tests

--- CoreAngular/ClientApp/src/app/scaffold/scaffold-container/scaffold-container.component.spec.ts
+++ CoreAngular/ClientApp/src/app/scaffold/scaffold-container/scaffold-container.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ScaffoldContainerComponent } from './scaffold-container.component';
 
 describe('ScaffoldContainerComponent', () => {
+  let element: HTMLElement;
   let component: ScaffoldContainerComponent;
   let fixture: ComponentFixture<ScaffoldContainerComponent>;
 
@@ -17,10 +18,15 @@ describe('ScaffoldContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ScaffoldContainerComponent);
     component = fixture.componentInstance;
+    element = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    element.remove();
   });
 });
