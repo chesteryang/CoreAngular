e83e22f9d4f59d9644ec2ae24f27743b883c522f
Chester Yang <chester_cyyang@hotmail.com>
2018-07-24 08:38:44 -0600

    pass unit tests

--- CoreAngular/ClientApp/src/app/scaffold/reactive-form/reactive-form.component.spec.ts
+++ CoreAngular/ClientApp/src/app/scaffold/reactive-form/reactive-form.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgReduxTestingModule, MockNgRedux } from '@angular-redux/store/lib/testing';
 import { ReactiveFormComponent } from './reactive-form.component';
 import { UserInfoActions } from '../redux/actions';
+import { initUserInfo } from '../redux/reducers';
 
 describe('ReactiveFormComponent', () => {
   let component: ReactiveFormComponent;
@@ -9,7 +11,15 @@ describe('ReactiveFormComponent', () => {
 
   beforeEach(async(() => {
     const spy = jasmine.createSpyObj('UserInfoActions', ['saveInfo']);
+    MockNgRedux.reset();
+    const mockStore = MockNgRedux.getSubStore();
+    const newsSelector = mockStore.getSelectorStub(['scaffoldState', 'userInfo']);
+    newsSelector.next([{userInfo: initUserInfo}]);
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        NgReduxTestingModule
+      ],
       declarations: [ ReactiveFormComponent ],
       providers: [
         { provide: UserInfoActions, useValue: spy}
