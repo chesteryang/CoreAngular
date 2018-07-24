e112c5ba88ef1ce210ea37a5e432f64aa0c8cfe8
Chester Yang <chester_cyyang@hotmail.com>
2018-07-23 21:12:34 -0600

    add reactive form

--- CoreAngular/ClientApp/src/app/scaffold/reactive-form/reactive-form.component.ts
+++ CoreAngular/ClientApp/src/app/scaffold/reactive-form/reactive-form.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { select } from '@angular-redux/store';
 import { IUserInfo } from '../redux/common';
 import { UserInfoActions } from '../redux/actions';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -10,11 +11,22 @@ import { UserInfoActions } from '../redux/actions';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
+  user: FormGroup;
 
   @select(['scaffoldState', 'userInfo']) readonly userInfo$: Observable<IUserInfo>;
-  constructor(private actions: UserInfoActions) { }
+
+  constructor(
+    private actions: UserInfoActions,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.userInfo$.subscribe((info) => {
+      this.user = this.fb.group({
+        name: [info.name],
+        phone: [info.phone],
+        email: [info.email]
+      });
+    });
   }
 
   onSubmit({ value, valid }: { value: IUserInfo, valid: boolean }) {
