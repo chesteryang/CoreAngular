9440e14cc4bd129c5885e4d67d84abbce4809c5f
Chester Yang <chester_cyyang@hotmail.com>
2018-07-31 18:31:02 -0600

    fix broken tests

--- CoreAngular/ClientApp/src/app/shared/models.service.spec.ts
+++ CoreAngular/ClientApp/src/app/shared/models.service.spec.ts
@@ -1,18 +1,22 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { NgRedux } from '@angular-redux/store';
 import { MockNgRedux } from '@angular-redux/store/testing';
-import { EmployeeViewModelActions, CustomerViewModelActions } from '../adw/redux/actions';
+import { EmployeeViewModelActions,
+  CustomerViewModelActions,
+  ProductViewModelActions } from '../adw/redux/actions';
 import { ModelsService } from './models.service';
 
 describe('ModelsService', () => {
   const spy1 = jasmine.createSpyObj('EmployeeViewModelActions', ['loadModel']);
   const spy2 = jasmine.createSpyObj('CustomerViewModelActions', ['loadModel']);
+  const spy3 = jasmine.createSpyObj('ProductViewModelActions', ['loadModel']);
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         { provide: NgRedux, useFactory: MockNgRedux.getInstance },
         { provide: EmployeeViewModelActions, useValue: spy1},
         { provide: CustomerViewModelActions, useValue: spy2},
+        { provide: ProductViewModelActions, useValue: spy3},
         ModelsService
       ]
     });
